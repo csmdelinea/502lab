083a939efc039917064cdf259e3a46ed23e0e0f5
Chris McGinn <Chris.McGinn@delinea.com>
2024-07-10 10:21:44 -0400

    parameterize start

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.WebHost.UseKestrel(options =>
+//{
+//    options.
+//});//builder.WebHost.UseKestrel(options =>
+//{
+//    options.
+//});
+
 builder.Services.AddReverseProxy()
        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
@@ -8,9 +16,13 @@ builder.Logging.AddLog4Net();
 // This is the HTTP/2 endpoint to register this app as part of the cluster endpoint
 var url = builder.Configuration["Tunnel:Url"]!;
 
+//overload command line arg for url
+if (args.Length > 0 && Uri.TryCreate(args[0].ToString(), UriKind.Absolute, out var test))
+    url = test.ToString();
+
 builder.WebHost.UseTunnelTransport(url, options =>
 {
-    options.Transport = TransportType.WebSockets;
+    options.Transport = url.Contains("connect-h2") ? TransportType.HTTP2 : TransportType.WebSockets;
 });
 
 var app = builder.Build();
